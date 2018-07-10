c109328b37599353d8cf7647faec7801dce14193
Jeffrey LEE <jeffrey.lee@zingbox.com>
2018-07-10 09:15:44 -0700

    Add sendQuery function

--- index.js
+++ index.js
@@ -15,12 +15,45 @@ class ZBAthena {
    * For region, accessKeyId and secretAccessKey, please make sure they are given
    * in configs param or stored in ~/.aws/ folder locally as stated in aws documentation
    * https://aws.amazon.com/sdk-for-node-js/
+   *
    * @param {JSON} configs - Optional configurations as stated from aws-sdk library
    *                         (https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Athena.html#constructor-property)
    */
   constructor(configs = {}) {
     this._configs = Object.assign(DEFAULT_CONFIGS, configs);
     this._athena = new AWS.Athena(this._configs);
+  }
+
+  /**
+   * Promisfied aws Athena library function startQueryExecution.
+   * Resolving a JSON with QueryExecutionId as stated in aws documentation.
+   * https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Athena.html#startQueryExecution-property
+   *
+   * @method sendQuery(query)
+   * @param {JSON} params - params required by aws-sdk library with format stated
+   *                        in documentation above.
+   *                        Simple example:
+   *                          var params = {
+   *                            QueryString: YOUR_QUERY, // Required
+                                ResultConfiguration: {
+                                  OutputLocation: YOUR_S3_OUTPUT_LOCATION // Required
+                                },
+                                QueryExecutionContext: {
+                                  Database: YOUR_ATHENA_DB_NAME
+                                }
+                              };
+   * @return {promise} A promise that resolve query request id or reject error
+   */
+  sendQuery(params) {
+    return new Promise((resolve, reject) => {
+      this._athena.startQueryExecution(params, (err, data) => {
+        if (err) {
+          reject(err); // an error occurred
+        } else {
+          resolve(data); // successfully queue up
+        }
+      });
+    });
   }
 
 }
